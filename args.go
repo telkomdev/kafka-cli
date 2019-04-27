4a0a96a46283b9491f97bd3006207dbe3c791ca0
wuriyanto <wuriyanto48@yahoo.co.id>
2019-04-27 16:58:39 +0700

    add argument paser

---
+++ args.go
@@ -0,0 +1,148 @@
+package kafka
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Argument struct
+type Argument struct {
+	Brokers     []string
+	Topic       string
+	Command     Command
+	ShowVersion bool
+	Help        func()
+	Message     []byte
+}
+
+type brokerList []string
+
+func (s *brokerList) String() string {
+	return fmt.Sprintf("%v", *s)
+}
+
+func (s *brokerList) Set(value string) error {
+	*s = strings.Split(value, ",")
+	return nil
+}
+
+// ParseArgument function
+func ParseArgument() (*Argument, error) {
+
+	var (
+		brokers     brokerList
+		topic       string
+		message     string
+		showVersion bool
+	)
+
+	argument := &Argument{}
+
+	// sub command
+	publishCommand := flag.NewFlagSet("pub", flag.ExitOnError)
+	subscribeCommand := flag.NewFlagSet("sub", flag.ExitOnError)
+
+	publishCommand.Var(&brokers, "b", "kafka brokers (you can add multiple brokers using separated comma eg: -b localhost:9091,localhost:9092 ..)")
+	publishCommand.Var(&brokers, "broker", "kafka brokers (you can add multiple brokers using separated comma eg: -b localhost:9091,localhost:9092 ..)")
+	publishCommand.StringVar(&topic, "t", "", "kafka topic")
+	publishCommand.StringVar(&topic, "topic", "", "kafka topic")
+	publishCommand.StringVar(&message, "-m", "", "message to publish")
+	publishCommand.StringVar(&message, "-message", "", "message to publish")
+
+	subscribeCommand.Var(&brokers, "b", "kafka brokers (you can add multiple brokers using separated comma eg: -b localhost:9091,localhost:9092 ..)")
+	subscribeCommand.Var(&brokers, "broker", "you can add multiple brokers using separated comma eg: -b localhost:9091,localhost:9092 ..)")
+	subscribeCommand.StringVar(&topic, "t", "", "kafka topic")
+	subscribeCommand.StringVar(&topic, "topic", "", "kafka topic")
+
+	flag.BoolVar(&showVersion, "version", false, "show version")
+
+	flag.Usage = func() {
+		fmt.Println()
+		fmt.Println("usage : kafka-cli pub -broker localhost:9092 -topic my-topic")
+		fmt.Println("sub command either pub (publish) or sub (subsriber)")
+		fmt.Println("-b | -broker : kafka brokers (you can add multiple brokers using separated comma eg: -b localhost:9091,localhost:9092 ..)")
+		fmt.Println("-t | -topic : kafka topic")
+		fmt.Println("-h show help")
+		fmt.Println("-version show version")
+	}
+
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		argument.Help = flag.Usage
+		return argument, errors.New("require at least one arguments")
+	}
+
+	switch os.Args[1] {
+	case "pub":
+		publishCommand.Parse(os.Args[2:])
+	case "sub":
+		subscribeCommand.Parse(os.Args[2:])
+	default:
+		argument.Help = flag.Usage
+		return argument, errors.New("invalid command")
+	}
+
+	// publish command parsed
+	if publishCommand.Parsed() {
+
+		if len(brokers) <= 0 {
+			argument.Help = flag.Usage
+			return argument, errors.New("require at least one broker")
+		}
+
+		if len(topic) <= 0 {
+			argument.Help = flag.Usage
+			return argument, errors.New("require a topic name")
+		}
+
+		if len(message) <= 0 {
+			argument.Help = flag.Usage
+			return argument, errors.New("pub command require a message")
+		}
+
+		for _, broker := range brokers {
+			if broker == "" {
+				continue
+			}
+
+			argument.Brokers = append(argument.Brokers, broker)
+		}
+
+		argument.Topic = topic
+		argument.Message = []byte(message)
+		argument.Command = CommandFromString(os.Args[1])
+	}
+
+	// subscribe command parsed
+	if subscribeCommand.Parsed() {
+
+		if len(brokers) <= 0 {
+			argument.Help = flag.Usage
+			return argument, errors.New("require at least one broker")
+		}
+
+		if len(topic) <= 0 {
+			argument.Help = flag.Usage
+			return argument, errors.New("require a topic name")
+		}
+
+		for _, broker := range brokers {
+			if broker == "" {
+				continue
+			}
+
+			argument.Brokers = append(argument.Brokers, broker)
+		}
+
+		argument.Topic = topic
+		argument.Command = CommandFromString(os.Args[1])
+	}
+
+	argument.ShowVersion = showVersion
+
+	return argument, nil
+}
