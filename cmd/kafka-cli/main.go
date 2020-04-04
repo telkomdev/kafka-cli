ebb61bace9bf39db49699aa5f6feb753fdbdf713
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-04-04 15:59:09 +0700

    update bin, to use sarama

--- cmd/kafka-cli/main.go
+++ cmd/kafka-cli/main.go
@@ -30,14 +30,28 @@ func main() {
 	}
 
 	ctx := context.Background()
-	publisher, err := kafka.NewKafkaGoPublisher(args.Topic, args.Brokers...)
+	// publisher, err := kafka.NewKafkaGoPublisher(args.Topic, args.Brokers...)
+	// if err != nil {
+	// 	fmt.Println("error : ", err)
+
+	// 	os.Exit(1)
+	// }
+
+	// subsriber, err := kafka.NewKafkaGoSubscriber(args.Topic, args.Brokers...)
+	// if err != nil {
+	// 	fmt.Println("error : ", err)
+
+	// 	os.Exit(1)
+	// }
+
+	publisher, err := kafka.NewSaramaPublisher(args.Brokers...)
 	if err != nil {
 		fmt.Println("error : ", err)
 
 		os.Exit(1)
 	}
 
-	subsriber, err := kafka.NewKafkaGoSubscriber(args.Topic, args.Brokers...)
+	subsriber, err := kafka.NewSaramaSubscriber(args.Brokers...)
 	if err != nil {
 		fmt.Println("error : ", err)
 
